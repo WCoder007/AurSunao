f5041c5f462d142b010c539a6b2f5d13dcdcb32c
Niveditha N <nniveditha2001@gmail.com>
2021-05-30 05:02:04 +0530

    Paras ka changes

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# AurSunao
-In times you feel all alone, now you too have someone to say "Aur Sunao"
+## AurSunao
+
+### Employee Referral Application - Team OneMoreHack
+#### In times you feel all alone, now you too have someone to say "Aur Sunao!"
+
+### Setup for developers
+Follow the instructions below to run the application in developer mode:
+1. Clone the repository
+
+    `git clone https://github.com/WCoder007/AurSunao.git`
+2. Install [flutter](https://flutter.dev) and set up the evironment.
+3. Run the application
+
+    `flutter run --no-sound-null-safety`
--- lib/sidebar/sidebar.dart
+++ lib/sidebar/sidebar.dart
@@ -115,7 +115,8 @@ class _SideBarState extends State<SideBar>
                                   "https://aursunaobackend.herokuapp.com/retrieve?user=" +
                                       widget.userid;
                               final response = await http.get(Uri.parse(uri));
-                              final result = json.decode(response.body) as Map<String,List>;
+                              final result = jsonDecode(response.body);
+                              // as Map<String, List<String>>;
                               print(result);
                               Navigator.push(
                                   context,
